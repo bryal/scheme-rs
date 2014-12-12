491350064774989f02dd69328edacdbd106135d7
bryal <96.bryal@gmail.com>
2014-12-12 20:44:31 +0100

    Added interactive shell
    Changed cmdline parameters to accomodate for interactive shell.
    Added support for CR-LF (Windows) line endings. Works both in shell and from file.

--- src/cmdline.rs
+++ src/cmdline.rs
@@ -3,32 +3,37 @@ use std::os;
 use std::io::File;
 
 fn print_usage(program: &str, opts: &[OptGroup]) {
-	println!("{}", usage(format!("Usage: {} <S-EXPR | -f FILE> [options]",
-			program).as_slice(),
-		opts));
+	let usage = usage(format!("Usage: {} [FILE | -s S-EXPR] [options]\n \
+			Leave empty for interactive shell", program).as_slice(), opts);
+	println!("{}", usage);
 }
 
-pub fn get_input() -> String {
+/// Get source code to parse and execute from commandline, file, or if no text supplied:
+/// return `None` to signal for interactive shell
+pub fn get_input() -> Option<String> {
 	let args: Vec<String> = os::args();
 
 	let program = args[0].clone();
 	let opts = [
-		optopt("f", "", "read lisp from a file", "FILE"),
+		optopt("s", "", "read lisp from supplied string", "S-EXPR"),
 		optflag("h", "help", "print this help menu")
 	];
 	let matches = match getopts(args.tail(), &opts) {
 		Ok(m) => m,
-		Err(f) => { panic!(f.to_string()) }
+		Err(f) => { panic!("{}", f) }
 	};
 	if matches.opt_present("h") {
 		print_usage(program.as_slice(), &opts);
 		panic!();
 	}
-	let input_lisp = match matches.opt_str("f") {
-		Some(x) => String::from_utf8(
-			File::open(&Path::new(x)).read_to_end().ok().unwrap()
-			).ok().unwrap(),
-		None => matches.free[0].clone()
+	let source = match matches.opt_str("s") {
+		Some(s) => Some(s),
+		None if matches.free.len() == 1 => {
+			let file_path = &Path::new(matches.free[0].clone());
+			let file_bytes = File::open(file_path).read_to_end().unwrap();
+			Some(String::from_utf8(file_bytes).unwrap())
+		},
+		_ => None
 	};
-	input_lisp
+	source
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,40 @@ extern crate getopts;
 use std::mem::transmute;
 use std::f64::consts::PI;
 use std::fmt;
+use std::io;
 use SEle::*;
-
+use SchemeAlertType as SAT;
+use SchemeAlert as SA;
 mod cmdline;
+
+
+enum SchemeAlertType {
+	Warn,
+	Error
+}
+
+impl fmt::Show for SchemeAlertType {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			SAT::Warn => write!(f, "Warning"),
+			SAT::Error => write!(f, "Error")
+		}
+	}
+}
+
+enum SchemeAlert<'a> {
+	Unexp(&'a str),
+	Undef(&'a str),
+}
+
+fn scheme_alert(alert: SA, a_type: SchemeAlertType) {
+	match alert {
+		SA::Unexp(s) => println!("; {}: Unexpected `{}`", a_type, s),
+	}
+	if let SAT::Error = a_type {
+		panic!()
+	}
+}
 
 /// An element in an S-Expression list
 #[deriving(Clone)]
@@ -52,15 +83,26 @@ impl fmt::Show for SEle {
 	}
 }
 
+fn unity() -> SEle {
+	SPair(None)
+}
+
 type List = Vec<SEle>;
 
-fn split_source(s: &str) -> Vec<&str> {
+fn split_source_text(s: &str) -> Vec<&str> {
 	let mut ss = Vec::with_capacity(s.len() / 4);
 	let mut start_pos = 0;
 	let (mut in_string, mut in_escape, mut in_comment) = (false,false,false);
 	let mut in_space = true;
-	for (i, c) in s.char_indices() {
-		// Check for comment
+	let mut char_iter = s.char_indices();
+	loop {
+		// Manual iteration in case we have to skip an iteration sometime
+		let (i, c) = match char_iter.next() {
+			Some(p) => p,
+			None => break
+		};
+
+		// First pass: Check for comment
 		match c {
 			'\n' => in_comment = false,
 			';' if !in_string && !in_escape => in_comment = true,
@@ -70,7 +112,7 @@ fn split_source(s: &str) -> Vec<&str> {
 			start_pos = i+1;
 			continue
 		}
-		// General splitting
+		// Second pass: General splitting
 		match c {
 			' ' => {
 				if !in_string {
@@ -81,12 +123,18 @@ fn split_source(s: &str) -> Vec<&str> {
 				}
 				if in_escape { in_escape = false; }
 			},
-			'\n' => {
+			'\r' | '\n' => {
 				if !in_string {
 					if !in_space && start_pos != i{
 						ss.push(s.slice(start_pos, i));
 					}
-					start_pos = i+1;
+					// Handle \r\n line endings in case of windows
+					if c == '\r' {
+						start_pos = i + 2;
+						char_iter.next();
+					} else {
+						start_pos = i + 1;
+					}
 				}
 				if in_escape { in_escape = false; }
 			},
@@ -109,7 +157,7 @@ fn split_source(s: &str) -> Vec<&str> {
 			},
 			_ => (),
 		}
-		// Check for space
+		// Third pass: Check for space
 		match c {
 			' ' | '\n' => in_space = true,
 			_ => in_space = false
@@ -133,6 +181,19 @@ fn matching_paren(v: &[&str]) -> Option<uint> {
 		}
 	}
 	None
+}
+
+/// How many more closing parens there are than opening parens
+fn right_paren_surplus(v: &[&str]) -> i32 {
+	let mut r_surplus: i32 = 0;
+	for s in v.iter() {
+		if *s == ")" {
+			r_surplus += 1;
+		} else if *s == "(" {
+			r_surplus -= 1;
+		}
+	}
+	r_surplus
 }
 
 fn parse_binding(s: &str) -> Option<String> {
@@ -173,7 +234,7 @@ fn parse_bool(s: &str) -> Option<bool> {
 	}
 }
 
-fn parse_expression(unparsed: &[&str]) -> List {
+fn parse_expressions(unparsed: &[&str]) -> List {
 	let mut parsed = Vec::with_capacity(unparsed.len());
 	let mut i = 0;
 	while i < unparsed.len() {
@@ -182,7 +243,7 @@ fn parse_expression(unparsed: &[&str]) -> List {
 			let matching_paren = matching_paren(
 					unparsed.slice_from(i+1)
 				).expect("Unclosed delimiter");
-			parsed.push(SExpr(parse_expression(
+			parsed.push(SExpr(parse_expressions(
 						unparsed.slice(i+1,
 							i+1+matching_paren))));
 			i = i+matching_paren+1;
@@ -202,14 +263,18 @@ fn parse_expression(unparsed: &[&str]) -> List {
 	parsed
 }
 
-fn parse_source(src: String) -> List {
-	let splitted = split_source(src.as_slice());
-	parse_expression(splitted.as_slice())
+fn parse_source_text(src: String) -> List {
+	let splitted = split_source_text(src.as_slice());
+	parse_expressions(splitted.as_slice())
+}
+
+fn into_begin(s: String) -> String {
+	format!("(begin {})", s)
 }
 
 fn scm_add_iter(env: &mut Env, sum: f64, mut operands: List) -> f64 {
 	if let Some(expr) = operands.pop() {
-		match env.exp_value(expr) {
+		match env.elem_value(expr) {
 			SNum(x) => scm_add_iter(env, sum + x, operands),
 			x => panic!("`{}` is NaN", x)
 		}
@@ -229,6 +294,12 @@ struct FnDef<'a> {
 	body: Option<SEle>
 }
 
+impl<'a> fmt::Show for FnDef<'a> {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "{}", self.name)
+	}
+}
+
 impl<'a> FnDef<'a> {
 	fn new_basic(name: String, cls: ScmFn<'a>) -> FnDef<'a> {
 		FnDef{name: name, arg_names: None, closure: cls, body: None}
@@ -246,10 +317,6 @@ struct Env<'a> {
 	var_defs: Vec<(String, SEle)>
 }
 
-fn unity() -> SEle {
-	SPair(None)
-}
-
 fn call_fn(env: *mut Env, f_name: &str, args: List) -> SEle {
 	let env_brw: &mut Env = unsafe{transmute(&mut *env)};
 	let fndef = env_brw.get_fn(f_name);
@@ -258,7 +325,7 @@ fn call_fn(env: *mut Env, f_name: &str, args: List) -> SEle {
 
 impl<'a> Env<'a> {
 	fn define_var(&mut self, binding: String, val: SEle) {
-		let val = self.exp_value(val);
+		let val = self.elem_value(val);
 		self.var_defs.push((binding, val));
 	}
 
@@ -271,7 +338,7 @@ impl<'a> Env<'a> {
 				continue;
 			}
 		}
-		panic!("Variable `{}` is not defined", to_get)
+		scheme_alert("Variable `{}` is not defined", to_get)
 	}
 
 	fn pop_var_def(&mut self) -> (String, SEle) {
@@ -288,6 +355,20 @@ impl<'a> Env<'a> {
 		}
 	}
 
+	fn pop_fn_def(&mut self) -> FnDef {
+		if let Some(f) = self.fn_defs.pop() {
+			f
+		} else {
+			panic!("Could not pop fn definition")
+		}
+	}
+
+	fn pop_fn_defs(&mut self, n: uint) {
+		for _ in range(0, n) {
+			self.pop_fn_def();
+		}
+	}
+
 	// Get the closure, argument names, and expression body for a procedure
 	fn get_fn(&mut self, f_name: &str) -> &'a mut FnDef {
 		for fndef in self.fn_defs.iter_mut().rev() {
@@ -298,28 +379,6 @@ impl<'a> Env<'a> {
 			}
 		}
 		panic!("Procedure `{}` is not defined", f_name)
-	}
-
-	fn define_fn(&mut self, head: SEle, body: SEle) {
-		let (fn_binding, arg_names) = self.dismantle_f_head(head);
-		if let SExpr(_) = body {} else {
-			panic!("`{}` is not a valid procedure body", body)
-		}
-		let defined_f: ScmFn<'a> = |env, arg_names_opt, ops, body| {
-			let arg_names = arg_names_opt.unwrap();
-			let n_args = arg_names.len();
-			if n_args != ops.len() {
-				panic!("Wrong number of arguments supplied");
-			}
-			// Bind and push supplied args to environment
-			for (k, v) in arg_names.clone().into_iter().zip(ops.into_iter()) {
-				env.define_var(k, v);
-			}
-			let result = env.exp_value(body.unwrap());
-			env.pop_var_defs(n_args);
-			result
-		};
-		self.fn_defs.push(FnDef::new_full(fn_binding, arg_names, defined_f, body))
 	}
 
 	// Extract the procedure name and argument names for the `define` header `head`
@@ -342,41 +401,81 @@ impl<'a> Env<'a> {
 		}
 	}
 
-	/// Evaluate the Scheme expression, assuming `expr` is a list of the
-	/// expression body
-	fn eval(&mut self, mut expr: List) -> SEle {
-		if let SBinding(s) = expr.remove(0).unwrap() {
-			let ele = call_fn(self, s.as_slice(), expr);
-
-			if let SExpr(inner_expr) = ele {
-				self.eval(inner_expr)
-			} else {
-				ele
+	fn define_fn(&mut self, head: SEle, body: SEle) {
+		let (fn_binding, arg_names) = self.dismantle_f_head(head);
+		let defined_f: ScmFn<'a> = |env, arg_names_opt, ops, body| {
+			let arg_names = arg_names_opt.unwrap();
+			let n_args = arg_names.len();
+			if n_args != ops.len() {
+				panic!("Wrong number of arguments supplied");
 			}
-		} else {
-			panic!("Not a procedure")
+			// Bind and push supplied args to environment
+			for (k, v) in arg_names.clone().into_iter().zip(ops.into_iter()) {
+				env.define_var(k, v);
+			}
+			let result = env.elem_value(body.unwrap());
+			env.pop_var_defs(n_args);
+			result
+		};
+		self.fn_defs.push(FnDef::new_full(fn_binding, arg_names, defined_f, body))
+	}
+
+	/// Evaluate the Scheme expression, assuming `expr` is a list of the
+	/// expression body starting with a procedure binding
+	fn eval_expr(&mut self, mut expr: List) -> SEle {
+		match expr.remove(0).unwrap() {
+			SBinding(s) => {
+				let ele = call_fn(self, s.as_slice(), expr);
+				if let SExpr(inner_expr) = ele {
+					self.eval_expr(inner_expr)
+				} else {
+					ele
+				}
+			},
+			x => panic!("`{}` is not a procedure", x)
 		}
 	}
 
-	fn exp_value(&mut self, ele: SEle) -> SEle {
+	// Get the value of the element. If it's an expression, get the evaluation. If it's a
+	// binding, get the associated value. If it's a value, return it.
+	fn elem_value(&mut self, ele: SEle) -> SEle {
 		match ele {
-			SExpr(x) => self.eval(x),
+			SExpr(x) => self.eval_expr(x),
 			SBinding(x) => self.get_var(x.as_slice()),
 			_ => ele
 		}
 	}
 
-	fn run_program(&mut self, top_lvl_exprs: List) {
-		for expr in top_lvl_exprs.into_iter() {
-			println!("\n___\n{}\n=", expr)
-			println!("{}", self.exp_value(expr));
+	fn eval_multiple(&mut self, exprs: List) -> SEle {
+		let mut it = exprs.into_iter();
+		if it.len() == 0 {
+			return unity();
 		}
+		for expr in range(0, it.len()-1).map(|_| it.next()) {
+			self.elem_value(expr.unwrap());
+		}
+		self.elem_value(it.next().expect("Failed to retrieve last expression"))
+	}
+
+	// This is run when there are multiple statements in a single expression body, like in a
+	// `begin`. Only in these situations can variables and procedures be `define`d, wherefore
+	// this method keeps track of variables and procedures in scope.
+	fn eval_block(&mut self, exprs: List) -> SEle {
+		let (previous_vars, previous_fns) = (self.var_defs.len(), self.fn_defs.len());
+
+		let result = self.eval_multiple(exprs);
+
+		let (current_vars, current_fns) = (self.var_defs.len(), self.fn_defs.len());
+		self.pop_var_defs(current_vars - previous_vars);
+		self.pop_fn_defs(current_fns - previous_fns);
+
+		result
 	}
 }
 
 fn scm_sub_iter(env: &mut Env, diff: f64, mut operands: List) -> f64 {
 	if let Some(expr) = operands.pop() {
-		match env.exp_value(expr) {
+		match env.elem_value(expr) {
 			SNum(x) => scm_sub_iter(env, diff - x, operands),
 			x => panic!("`{}` is NaN", x)
 		}
@@ -387,7 +486,7 @@ fn scm_sub_iter(env: &mut Env, diff: f64, mut operands: List) -> f64 {
 
 fn scm_div_iter(env: &mut Env, numerator: f64, mut denoms: List) -> f64 {
 	if let Some(expr) = denoms.pop() {
-		match env.exp_value(expr) {
+		match env.elem_value(expr) {
 			SNum(x) => scm_div_iter(env, numerator / x, denoms),
 			x => panic!("`{}` is NaN", x)
 		}
@@ -396,24 +495,67 @@ fn scm_div_iter(env: &mut Env, numerator: f64, mut denoms: List) -> f64 {
 	}
 }
 
+fn to_strings(slc: &[&str]) -> Vec<String> {
+	slc.iter().map(|s| s.to_string()).collect()
+}
+
+fn to_strs(slc: &[String]) -> Vec<&str> {
+	slc.iter().map(|s| s.as_slice()).collect()
+}
+
+fn interactive_shell(env: &mut Env) {
+	print!("scheme-rs interactive shell. Copyright (C) 2014  Johan Johansson\n\
+		This program is free software released under the GPLv3 license.\n\n\
+		>> ");
+
+	let mut token_buf = Vec::with_capacity(100);
+	let mut rparen_surplus = 0;
+	for line in io::stdin().lock().lines() {
+		let line = line.unwrap();
+		let splitted = split_source_text(line.as_slice().clone());
+		rparen_surplus += right_paren_surplus(splitted.as_slice());
+		match rparen_surplus {
+			n if rparen_surplus < 0 => {
+				token_buf.push_all(to_strings(splitted.as_slice()).as_slice());
+				print!("\n>> {}", String::from_char((n * -2) as uint, ' '))
+			},
+			_ if rparen_surplus > 0 => {
+				scheme_alert(SA::Unexp(")"), SAT::Warn);
+				token_buf.clear();
+				rparen_surplus = 0;
+				print!(">> ");
+			},
+			_ => {
+				token_buf.push_all(to_strings(splitted.as_slice()).as_slice());
+				let parsed = parse_expressions(to_strs(token_buf.as_slice()).as_slice());
+				print!("{}\n>> ", env.eval_multiple(parsed));
+				token_buf.clear();
+				rparen_surplus = 0;
+			}
+		}
+	}
+}
+
 fn main(){
 	// Scheme Standard library
 	let std_procs: Vec<(&str, ScmFn)> = vec![
 		("+", |env: &mut Env, _: Option<Vec<String>>, ops, _: Option<SEle>|
-			SNum(scm_add_iter(env, 0.0, ops))),
+			SNum(scm_add_iter(env, 0.0, ops)
+		)),
 		("-", |env: &mut Env, _: Option<Vec<String>>, mut ops, _: Option<SEle>|
 			SNum(if let Some(expr) = ops.remove(0) {
-				if let SNum(x) = env.exp_value(expr) {
+				if let SNum(x) = env.elem_value(expr) {
 					scm_sub_iter(env, x, ops)
 				} else {
 					panic!("NaN")
 				}
 			} else {
 				0.0
-			})),
+			}
+		)),
 		("/", |env: &mut Env, _: Option<Vec<String>>, mut ops, _: Option<SEle>|
 			SNum(if let Some(expr) = ops.remove(0) {
-				if let SNum(x) = env.exp_value(expr) {
+				if let SNum(x) = env.elem_value(expr) {
 					if ops.len() < 1 {
 						1.0 / x
 					} else {
@@ -424,7 +566,8 @@ fn main(){
 				}
 			} else {
 				panic!("Wrong number of arguments")
-			})),
+			}
+		)),
 		("define", |env: &mut Env, _: Option<Vec<String>>, mut ops, _: Option<SEle>| {
 			if ops.len() != 2 {
 				panic!("Wrong number of arguments")
@@ -437,15 +580,27 @@ fn main(){
 			unity()
 				
 		}),
+		("display", |env: &mut Env, _: Option<Vec<String>>, mut ops, _: Option<SEle>| {
+			if ops.len() != 1 {
+				panic!("Arity missmatch")
+			}
+			println!("{}", env.elem_value(ops.pop().expect(
+				"Could not retrieve argument")));
+			unity()
+		}),
+		("begin", |env: &mut Env, _: Option<Vec<String>>, ops, _: Option<SEle>| {
+			env.eval_block(ops)
+		}),
 	];
 	let std_vars = vec![
 		("PI", SNum(PI)),
 	];
-	let (mut procs, mut vars) = (Vec::with_capacity(std_procs.len()), Vec::with_capacity(std_vars.len()));
-	for (n,cls) in std_procs.into_iter() {
-		procs.push(FnDef::new_basic(n.to_string(), cls));
+	let mut procs = Vec::with_capacity(std_procs.len());
+	let mut vars = Vec::with_capacity(std_vars.len());
+	for (name, cls) in std_procs.into_iter() {
+		procs.push(FnDef::new_basic(name.to_string(), cls));
 	}
-	for (k,v) in std_vars.into_iter() {
+	for (k, v) in std_vars.into_iter() {
 		vars.push((k.to_string(), v));
 	}
 	let mut env = Env{
@@ -453,9 +608,15 @@ fn main(){
 		var_defs: vars,
 	};
 
-	let input = cmdline::get_input();
-	let parsed = parse_source(input);
-	println!("{}\n", parsed);
-	
-	env.run_program(parsed);
+	let maybe_input = cmdline::get_input();
+	if let Some(input) = maybe_input {
+		let mut parsed = parse_source_text(into_begin(input));
+		println!("{}\n", parsed);
+		if parsed.len() != 1 {
+			panic!("Parsed source is invalid")
+		}
+		env.elem_value(parsed.pop().expect("Failed to retrieve expression"));
+	} else {
+		interactive_shell(&mut env);
+	}
 }
